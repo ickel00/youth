f489f0198d7966435d331c3569d708c026107db0
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-04-01 18:16:57 +0800

    Add files via upload

--- Task/hlyy.js
+++ Task/hlyy.js
@@ -64,16 +64,13 @@ if(!$.isNode()&&hlyyhd.indexOf("\n") ==-1){
     } else {
         hlyyurl = [process.env.HLYY_URL]
     };
-    if (process.env.HLYY_BODY && process.env.HLYY_BODY.indexOf('\n') > -1) {
-        hlyybody = process.env.HLYY_BODY.split('\n');
-    } else {
-        hlyybody = [process.env.HLYY_BODY]
-    };	
+	const hlyybody = $.getdata('hlyybody');
+
     console.log(` ============脚本执行 - 北京时间 (UTC + 8)：${new Date(new Date().getTime() + 60 * 60 * 1000).toLocaleString()} =============\n`);
  } else if(!$.isNode()&&hlyyhd.indexOf("\n")>-1){
    hlyyhd = hlyyhd.split("&")
    hlyyurl = hlyyurl.split("&")
-   hlyybody = hlyybody.split("&")  
+   const hlyybody = $.getdata('hlyybody');  
 };
     Object.keys(hlyyhd).forEach((item) =>{
         if (hlyyhd[item]) {
@@ -85,11 +82,7 @@ if(!$.isNode()&&hlyyhd.indexOf("\n") ==-1){
             hlyyurlArr.push(hlyyurl[item])
         }
     });	
-    Object.keys(hlyybody).forEach((item) =>{
-        if (hlyybody[item]) {
-            hlyybodyArr.push(hlyybody[item])
-        }
-    });		
+    hlyybodyArr.push($.getdata('hlyybody'))
  console.log(` ============= 您共提供${hlyyhdArr.length}个葫芦音乐账号 =============`);
 }
 
