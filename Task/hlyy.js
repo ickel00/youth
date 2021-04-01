80a23dec4b2f410b34dfc5dbe206cec446f122f0
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-04-01 18:47:12 +0800

    Add files via upload

--- Task/hlyy.js
+++ Task/hlyy.js
@@ -64,13 +64,13 @@ if(!$.isNode()&&hlyyhd.indexOf("\n") ==-1){
     } else {
         hlyyurl = [process.env.HLYY_URL]
     };
-	hlyybody = hlyybody;
+	hlyybody = hlyybody.split("\n")
 
     console.log(` ============脚本执行 - 北京时间 (UTC + 8)：${new Date(new Date().getTime()).toLocaleString()} =============\n`);
  } else if(!$.isNode()&&hlyyhd.indexOf("\n")>-1){
-   hlyyhd = hlyyhd.split("&")
-   hlyyurl = hlyyurl.split("&")
-   hlyybody = hlyybody; 
+   hlyyhd = hlyyhd.split("\n")
+   hlyyurl = hlyyurl.split("\n")
+   hlyybody = hlyybody.split("\n")
 };
     Object.keys(hlyyhd).forEach((item) =>{
         if (hlyyhd[item]) {
