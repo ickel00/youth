6fd0e5127a44de3a027b3579242f3b3f51e4e4fd
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-04-05 19:23:33 +0800

    Add files via upload

--- Task/txnews.js
+++ Task/txnews.js
@@ -232,7 +232,7 @@ function treesign() {
  return new Promise((resolve, reject) => {
   treetoken = prizeVal.split("?")[1]
  let url = {
-     url: 'https://api.prize.qq.com/v1/newsapp/tree/sign?'+treetoken,
+     url: 'https://api.prize.qq.com/v1/newsapp/tree/querysign?'+treetoken,
      headers: Host().headers,
      body: "current_day="+Math.round(new Date(new Date().toLocaleDateString()).getTime()/1000).toString()
  }
