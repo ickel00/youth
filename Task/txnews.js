b3d709959523a0eede8d27707097480580501ed2
ikcc0 <73400507+ickel00@users.noreply.github.com>
2021-04-05 19:46:01 +0800

    Add files via upload

--- Task/txnews.js
+++ Task/txnews.js
@@ -101,11 +101,11 @@ if (isGetCookie) {
             await $.wait(1000);
             await StepsTotal();
             //await showmsg();
-            if ($.isNode() && process.env.TXNEWS_NOTIFY_CONTROL) {
-                if (readnum % notifyInterval == 0 && cashtotal > 2) {
-                    await notify.sendNotify($.name, subTile + '\n' + detail)
-                }
-            }
+            //if ($.isNode() && process.env.TXNEWS_NOTIFY_CONTROL) {
+                //if (readnum % notifyInterval == 0 && cashtotal > 2) {
+                    //await notify.sendNotify($.name, subTile + '\n' + detail)
+                //}
+            //}
         }
     }
 })()
